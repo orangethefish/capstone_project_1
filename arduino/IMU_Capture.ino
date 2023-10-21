2a269eb54fabbde6249ee4965eff26fb138af132
Nguyễn Duy Hòa <hoa.nguyen2020@hcmut.edu.vn>
2023-10-21 23:55:01 +0700

    kalman

--- arduino/IMU_Capture.ino
+++ arduino/IMU_Capture.ino
@@ -11,11 +11,20 @@ LSM6DS3 myIMU(I2C_MODE, 0x6A);  //I2C device address 0x6A
 #define CUTOFF_FREQUENCY 20
 #define INTERVAL_MS (1000 / (FREQUENCY_HZ + 1))
 #define SAMPLING_PERIOD (1/FREQUENCY_HZ)
-static unsigned long last_interval_ms = 0;
+#define RAD_TO_DEG 		57.2957795131f
 
+static unsigned long last_interval_ms = 0;
+float rollEstimate_rad;
+float pitchEstimate_rad;
+float rollAcc_rad  = 0.0f;
+float pitchAcc_rad = 0.0f;
+float rollGyr_rad = 0.0f;
+float pitchGyr_rad = 0.0f;
 double acc_rdg[3],gyr_rdg[3]; //0 is x, 1 is y, 2 is z
 double offset[6]={0.051,0.1042,-0.1294,-0.6362,-0.6586,0.9682};
 double alpha;
+int count=0;
+EKF ekf;
 
 void setup() {
   Serial.begin(115200);
@@ -26,7 +35,11 @@ void setup() {
     Serial.println("Device error");
   } else {
     Serial.println("Device OK!");
-    alpha = 1 / (2 * PI * CUTOFF_FREQUENCY * SAMPLING_PERIOD);
+    EKF_Init(&ekf, (float[]){0.1f, 0.1f}, (float[]){0.001f, 0.001f}, (float[]){0.011f, 0.011f, 0.011f});
+    double timeConstant = 1 / (2 * PI * CUTOFF_FREQUENCY);
+    alpha = timeConstant / (timeConstant + SAMPLING_PERIOD);
+    rollEstimate_rad  = 0.0f;
+    pitchEstimate_rad = 0.0f;
     for(int i=0;i<3;i++){
       acc_rdg[i]=0;
       gyr_rdg[i]=0;
@@ -45,17 +58,27 @@ void loop() {
     gyr_rdg[1]= alpha * gyr_rdg[1]  + (1 - alpha) * (myIMU.readFloatGyroX()  + offset[4]);
     gyr_rdg[0]= alpha * gyr_rdg[0]  + (1 - alpha) * (myIMU.readFloatGyroY()  + offset[3]);
     gyr_rdg[2]= alpha * gyr_rdg[2]  - (1 - alpha) * (myIMU.readFloatGyroZ()  + offset[5]);
-    Serial.print(acc_rdg[0], 4);
+    EKF_Predict(&ekf, gyr_rdg[0], gyr_rdg[1], gyr_rdg[2], SAMPLING_PERIOD);
+    if(count>=10){
+      EKF_Update(&ekf, acc_rdg[0], acc_rdg[1], acc_rdg[2]);
+      count=0;
+    }else{
+      count++;
+    }
+    // Serial.print(acc_rdg[0], 4);
+    // Serial.print('\t');
+    // Serial.print(acc_rdg[1], 4);
+    // Serial.print('\t');
+    // Serial.print(acc_rdg[2], 4);
+    // Serial.print('\t');
+    // Serial.print(gyr_rdg[0], 4);
+    // Serial.print('\t');
+    // Serial.print(gyr_rdg[1], 4);
+    // Serial.print('\t');
+    // Serial.println(gyr_rdg[2], 4);
+    Serial.print(ekf.phi_r * RAD_TO_DEG, 4);
     Serial.print('\t');
-    Serial.print(acc_rdg[1], 4);
-    Serial.print('\t');
-    Serial.print(acc_rdg[2], 4);
-    Serial.print('\t');
-    Serial.print(gyr_rdg[0], 4);
-    Serial.print('\t');
-    Serial.print(gyr_rdg[1], 4);
-    Serial.print('\t');
-    Serial.println(gyr_rdg[2], 4);
+    Serial.println(ekf.theta_r * RAD_TO_DEG, 4);
   }
 }
 
